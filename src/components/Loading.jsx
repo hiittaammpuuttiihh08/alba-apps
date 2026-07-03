4c908ad8e413f92ed0e8b5db326a9ad0d69fab80
yudhaph0300 <yudhapamungkas0300@gmail.com>
2026-07-03 15:41:58 -0700

    add loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,10 @@
 import "./loading.css";
 
 export default function Loading({ message = "Memuat...", size = "medium" }) {
-  return (
-    <div className={`loading loading--${size}`}>
-      <div className="loading__spinner" />
-      <div className="loading__message">{message}</div>
-    </div>
-  );
+   return (
+      <div className={`loading loading--${size}`}>
+         <div className="loading__spinner" />
+         <div className="loading__message">{message}</div>
+      </div>
+   );
 }
